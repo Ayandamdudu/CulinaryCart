efdb2bc925cb66a5302ae33e70ce1131fb8fce35
Ayandamdudu <159124907+Ayandamdudu@users.noreply.github.com>
2024-03-14 12:00:21 +0200

    Update menu.js

--- menu/menu.js
+++ menu/menu.js
@@ -1,38 +1,37 @@
-
-
+ 
 function showProductInfo(product) {
     // Store selected product info in sessionStorage
     sessionStorage.setItem('selectedProduct', product);
     // Redirect to product info page
     window.location.href = 'product_info.html';
   }
-
+ 
   // Retrieve selected product info from sessionStorage
 const selectedProduct = sessionStorage.getItem('selectedProduct');
-
+ 
 // Display product info based on the selected product
 const productInfoContainer = document.getElementById('productInfo');
 if (selectedProduct === 'Fish taco') {
   product1_details(selectedProduct)
-
+ 
 } else if (selectedProduct === 'seafood paella') {
   product2_details(selectedProduct)
-
+ 
 } else if (selectedProduct === 'pokebowl') {
   product3_details(selectedProduct)
-  
+ 
 } else if (selectedProduct === 'shrimpscampi') {
   product4_details(selectedProduct)
-
+ 
 }
-
+ 
 else {
   productInfoContainer.innerHTML = '<p>No product information available.</p>';
 }
-
+ 
 function product1_details(selectedProduct) {
     productInfoContainer.innerHTML = `
-    <h2>Product 1 Information</h2>
+    <h2>Fish taco Information</h2>
     <p>these are your the items for ${selectedProduct}.</p>
     <ul>
         <li>ingrediant 1</li>
@@ -42,10 +41,10 @@ function product1_details(selectedProduct) {
     </ul>
   `;
 }
-
+ 
 function product2_details(selectedProduct) {
     productInfoContainer.innerHTML = `
-    <h2>Product 2 Information</h2>
+    <h2>seafood paella Information</h2>
     <p>these are your the items for ${selectedProduct}.</p>
     <ul>
         <li>ingrediant 1</li>
@@ -59,10 +58,10 @@ function product2_details(selectedProduct) {
     </ul>
   `;
 }
-
+ 
 function product3_details(selectedProduct) {
   productInfoContainer.innerHTML = `
-  <h2>Product 1 Information</h2>
+  <h2>pokebowl Information</h2>
   <p>these are your the items for ${selectedProduct}.</p>
   <ul>
       <li>ingrediant 1</li>
@@ -74,10 +73,10 @@ function product3_details(selectedProduct) {
   </ul>
 `;
 }
-
+ 
 function product4_details(selectedProduct) {
   productInfoContainer.innerHTML = `
-  <h2>Product 1 Information</h2>
+  <h2>shrimpscampi Information</h2>
   <p>these are your the items for ${selectedProduct}.</p>
   <ul>
       <li>ingrediant 1</li>
@@ -87,7 +86,7 @@ function product4_details(selectedProduct) {
   </ul>
 `;
 }
-
+ 
 function product5_details(selectedProduct) {
   productInfoContainer.innerHTML = `
   <h2>Product 1 Information</h2>
@@ -99,4 +98,3 @@ function product5_details(selectedProduct) {
       <li>ingrediant 4</li>
   </ul>
 `;
-}
