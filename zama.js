8d93a876129a447acd4822e3e26bdf4f296508c9
yondela <yondela08@outlook.com>
2024-04-03 15:22:30 +0200

    added scroll functions for menu preview in home page

--- zama.js
+++ zama.js
@@ -1,47 +1,24 @@
-// Define arrays to store product details
-const productDetails = {
-    'Fish taco': {
-      title: 'Fish taco Information',
-      ingredients: ['ingredient 1', 'ingredient 2', 'ingredient 3', 'ingredient 4']
-    },
-    'seafood paella': {
-      title: 'Seafood Paella Information',
-      ingredients: ['ingredient 1', 'ingredient 2', 'ingredient 3', 'ingredient 4', 'ingredient 5', 'ingredient 6', 'ingredient 7', 'ingredient 8']
-    },
-    'pokebowl': {
-      title: 'Pokebowl Information',
-      ingredients: ['ingredient 1', 'ingredient 2', 'ingredient 3', 'ingredient 4', 'ingredient 5', 'ingredient 6']
-    },
-    'shrimpscampi': {
-      title: 'Shrimp Scampi Information',
-      ingredients: ['ingredient 1', 'ingredient 2', 'ingredient 3', 'ingredient 4']
-    }
-  };
-   
-  // Function to display product information
-  function showProductInfo(product) {
-    // Store selected product info in sessionStorage
-    sessionStorage.setItem('selectedProduct', product);
-    // Redirect to product info page
-    window.location.href = 'product_info.html';
-  }
-   
-  // Retrieve selected product info from sessionStorage
-  const selectedProduct = sessionStorage.getItem('selectedProduct');
-   
-  // Display product info based on the selected product
-  const productInfoContainer = document.getElementById('productInfo');
-   
-  // Check if selected product exists in the productDetails array
-  if (selectedProduct && productDetails[selectedProduct]) {
-    const { title, ingredients } = productDetails[selectedProduct];
-    productInfoContainer.innerHTML = `
-  <h2>${title}</h2>
-  <p>These are the items for ${selectedProduct}.</p>
-  <ul>
-        ${ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')}
-  </ul>
-    `;
-  } else {
-    productInfoContainer.innerHTML = '<p>No product information available.</p>';
-  }
+
+document.addEventListener('DOMContentLoaded', function () {
+  const container = document.querySelector('.scrollable-container');
+  const btnLeft = document.querySelector('.scroll-left-button');
+  const btnRight = document.querySelector('.scroll-right-button');
+
+  container.addEventListener('wheel', function (e) {
+    e.preventDefault();
+    container.scrollLeft += e.deltaY;
+  });
+
+  container.addEventListener('touchmove', function (e) {
+    e.preventDefault();
+    container.scrollLeft += e.touches[0].clientX - e.touches[1].clientX;
+  });
+
+  btnLeft.addEventListener('click', function() {
+    container.scrollBy({left: -100, behavior: 'smooth' });
+  });
+
+  btnRight.addEventListener('click', function() {
+    container.scrollBy({right: 100, behavior: 'smooth' });
+  });
+});
